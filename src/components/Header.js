692b5a6b609afdbe10144904add5ba1dde00ebf5
Tommaso <tommaso.guezze.2004@gmail.com>
2025-08-11 18:28:37 +0200

    t

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
         <Link to="/" className="logo">
-          <span className="logo-text">go2west</span>
+          <img src="/logo.jpg" alt="go2west" style={{width: '150px', height: '75px'}} className="logo-text" />
         </Link>
         
         <nav className="nav">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/destination/north-america" className="nav-link">North America</Link>
-          <Link to="/destination/mexico" className="nav-link">Mexico</Link>
-          <Link to="/destination/kenya" className="nav-link">Kenya</Link>
-          <Link to="/destination/japan" className="nav-link">Japan</Link>
-          <Link to="/destination/australia" className="nav-link">Australia</Link>
-          <Link to="/destination/united-states" className="nav-link">United States</Link>
+          <a href="/" className="nav-link">Home</a>
+          <a href="/destination/north-america" className="nav-link">North America</a>
+          <a href="/destination/mexico" className="nav-link">Mexico</a>
+          <a href="/destination/kenya" className="nav-link">Kenya</a>
+          <a href="/destination/japan" className="nav-link">Japan</a>
+          <a href="/destination/australia" className="nav-link">Australia</a>
+          <a href="/destination/united-states" className="nav-link">United States</a>
         </nav>
       </div>
     </header>
